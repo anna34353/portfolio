80b3cebf88e99f3263c88bfa95d82c89e9b6f85f
anna34353 <109056837+anna34353@users.noreply.github.com>
2022-07-14 17:01:04 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,20 @@
+// for (i = 1; i < 8; i++) {
+//   var skillTitle = document.getElementById(`a${i}`);
+//   skillTitle.addEventListener("click", function (e) {
+//     let svg = e.path[1].querySelector("svg");
+//     console.log(skillTitle);
+//     boom(svg);
+//   });
+// }
+
+// function boom(svg) {
+//   //   if (svg.classList.contains("none")) {
+//   //     svg.classList.remove("none");
+//   //   } else {
+//   //     svg.classList.add("none");
+//   //   }
+
+//   svg.classList.contains("none")
+//     ? svg.classList.remove("none")
+//     : svg.classList.add("none");
+// }
